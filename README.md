79b327784960cb436bf8a68b1e3b21ff257f825c
kowasaur <75297283+kowasaur@users.noreply.github.com>
2022-06-30 22:28:24 +1000

    create basic tests

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Umi
 
+Note that the .expected files are the expected output of the corresponding .umi files.
+
 ## Goals
 
 -   [x] Compiles to [CIL](https://en.wikipedia.org/wiki/Common_Intermediate_Language) (extremely limited currently)
---
+++ test.py
@@ -0,0 +1,49 @@
+#               run all of the tests:  python test.py 
+# record all of the expected outputs:  python test.py record
+
+import subprocess
+import sys
+import os
+from glob import glob
+from typing import Callable
+
+def error(path: str, error_message: str) -> None:
+    print(f"\n{path} {error_message}")
+    sys.exit(1)
+
+def compile_file(path: str) -> None:
+    if subprocess.run(["mono", "umi.exe", path]).returncode:
+        error(path, "failed to compile")
+    if subprocess.run(["ilasm", "output.il"], stdout=subprocess.DEVNULL).returncode:
+        error(path, "failed to assemble")
+
+def expected_path(path: str) -> str:
+    return f"{os.path.splitext(path)[0]}.expected"
+
+def test_output(path: str, output: str) -> None:
+    with open(expected_path(path), "r") as f:
+        expected = f.read()
+    if expected != output:
+        print(output)
+        error(path, "created the above output, which was unexpected")
+
+def record_output(path: str, output: str) -> None:
+    with open(expected_path(path), "w") as f:
+        f.write(output)
+
+def affect_all(output_func: Callable[[str, str], None]) -> None:
+    for directory in ["examples", "tests"]:
+        for path in glob(f"{directory}/*.umi"):
+            compile_file(path)
+            output = subprocess.check_output(["mono", "output.exe"]).decode("utf-8")
+            output_func(path, output)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    subprocess.run(["mcs", "umi.cs"], stdout=subprocess.DEVNULL, check=True)
+    if len(args) == 0:
+        affect_all(test_output)
+        print("Everything passed succeessfully")
+    else:
+        affect_all(record_output)
+        print("Finished recording output successfully")
